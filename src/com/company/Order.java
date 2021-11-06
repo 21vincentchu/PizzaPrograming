cbe548b6e7c3fa7820ab0c40e3adfd55c84f3700
Vincent Chu <90657391+21vincentchu@users.noreply.github.com>
2021-11-05 20:11:28 -0500

    Update Order.java

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -6,16 +6,16 @@ import static com.company.Validation.validateInput;
 public class Order
 {
     public static ArrayList<String> orderform = new ArrayList<>();
-    public static boolean active = true;
+    //public static boolean active = true;
 
     public static void order(Scanner sc)
     {
         border(50);
         System.out.println("Options");
         System.out.println("1: Select Crust");
-        System.out.println("2: Select Sause");
+        System.out.println("2: Select Sauce");
         System.out.println("3: Select Toppings");
-        System.out.println("4: Exit");
+        System.out.println("4: See current Order");
 
         int option = validateInput(sc, 1, 4,"Not an option", "Select Between 1-4");
         switch(option)
@@ -30,76 +30,155 @@ public class Order
                 orderToppings(sc);
                 break;
             case 4:
-                active = false;
-                break;
+                System.out.println(orderform);
+                order(sc);
         }
     }
+
     public static void orderCrust(Scanner sc)
     {
-        System.out.println("Entered Crust");
+        System.out.println("Please Choose One Crust Option");
         border(50);
         System.out.println("Options");
         System.out.println("1: Regular Crust");
         System.out.println("2: Gluten Free Crust");
-        System.out.println("3: Return to order menu");
-        System.out.println("4: exit");
 
-        int option = validateInput(sc, 1, 4, "Not an option", "Enter 1 - 4");
+        int option = validateInput(sc, 1, 2, "Not an option", "Enter 1 - 2");
         switch(option)
         {
             case 1:
-                System.out.println("Regular Crust Added");
+                System.out.println("REGULAR CRUST ADDED");
                 orderform.add("Regular Crust");
                 break;
             case 2:
-                System.out.println("Gluten Free Crust Added");
+                System.out.println("GLUTEN FREE CRUST ADDED");
                 orderform.add("Gluten Free Crust");
-                break;
-            case 3:
-                order(sc);
-            case 4:
-                active = false;
                 break;
         }
     }
 
     public static void orderSauce(Scanner sc)
     {
-        System.out.println("Entered Sauce");
+        System.out.println("Please Choose One Sauce Option");
         border(50);
         System.out.println("options");
         System.out.println("1: White Sauce");
-        System.out.println("2:Tomato Sause");
-        System.out.println("3: Return to Order Menu");
-        System.out.println("4: Exit");
+        System.out.println("2: Tomato Sauce");
+        System.out.println("3: Balsamic Vinegar");
 
-        int option = validateInput(sc, 1, 4,"Not an option", "Select between 1 -4");
-
+        int option = validateInput(sc, 1, 3,"Not an option", "Select between 1 -3");
         switch(option)
         {
             case 1:
-                System.out.println("White sause added");
+                System.out.println("WHITE SAUCE ADDED");
                 orderform.add("White Sauce");
                 break;
             case 2:
-                System.out.println("Tomato Sauce Added");
-                orderform.add("Tomato Sause");
+                System.out.println("TOMATO SAUCE ADDED");
+                orderform.add("Tomato sauce");
                 break;
             case 3:
-                order(sc);
-                break;
-            case 4:
-                active = false;
-                break;
+                System.out.println("BALSAMIC VINEGAR ADDED");
+                orderform.add("Balsamic Vinegar");
         }
 
     }
 
     public static void orderToppings(Scanner sc)
     {
-        System.out.println("Entered Toppings");
+        boolean contine = true;
+        System.out.println("Please choose one ingredient option");
+        System.out.println("1: pizza cheese \t 2: Diced onion 3: Diced Green Onion");
+        int option = validateInput(sc, 1, 12, "Not an option", "Select between 1 -12");
+        switch(option)
+        {
+            case 1:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("pizza cheese");
+                orderform.add("pizza cheese");
+                break;
+            case 2:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("Diced Onions");
+                orderform.add("Diced Onions");
+                break;
+            case 3:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("Diced Green Onions");
+                orderform.add("Diced Green Onions");
+                break;
+            case 4:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("Pepperoni");
+                break;
+            case 5:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("Sliced Mushrooms");
+                break;
+            case 6:
+                System.out.println("Please choose one amount\n1: 1/4 cup \t\t 2: 1/2 cup");
+                printcup(sc);
+                System.out.print("diced jalapenos");
+                break;
+            case 7:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("sardines");
+                break;
+            case 8:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("pineapple chuncks");
+                break;
+            case 9:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("tofu");
+                break;
+            case 10:
+                System.out.println(printChoose());
+                printcup(sc);
+                System.out.print("Ham Chunks");
+                break;
+            case 11:
+                System.out.println("Please choose one amount\n1: 1/4 cup \t\t 2: 1/2 cup");
+                printcup(sc);
+                System.out.print("Dry Red pepper");
+                break;
+            case 12:
+                System.out.println("Please choose one amount\n1: 1/4 cup \t\t 2: 1/2 cup");
+                printcup(sc);
+                System.out.print("dry basil");
+                break;
 
 
+        }
+    }
+
+    public static void printcup(Scanner sc)
+    {
+        int option = validateInput(sc, 1, 2,"Not an option", "Select between 1 -2");
+        String cup1 = "1/4 cup";
+        String cup2 = "1/2 cup";
+        switch(option)
+        {
+            case 1:
+                System.out.print("You choose 1/4 cup of ");
+                break;
+            case 2:
+                System.out.println("You choose 1/2 cup of ");
+                break;
+        }
+    }
+
+    public static String printChoose()
+    {
+        return "Please choose one amount\n1: 1/4 cup \t\t 2: 1/2 cup";
     }
 
     public static void border (int length)
