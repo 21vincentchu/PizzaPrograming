2982cdb5a3beaaeebd275c54d6e674d1bbca45ae
Vincent Chu <90657391+21vincentchu@users.noreply.github.com>
2021-11-10 00:52:28 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,12 @@ import static com.company.Order.*;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Pizza Project! You will be making you own Pizza!");
-        //while(Order.active)
-       // {
-            //orderCrust(sc);
-            //orderSauce(sc);
-            orderToppings(sc);
-        //}
-        System.out.println("\n" + Order.orderform);
+        orderCrust(sc);
+        orderSauce(sc);
+        orderToppings(sc);
+        System.out.println("\nThis is your pizza order!" + Order.orderform);
     }
 }
-
