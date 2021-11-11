b7dc227835197eee0098a8b37bae226683b2e57e
Vincent Chu <90657391+21vincentchu@users.noreply.github.com>
2021-11-10 22:42:26 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,11 @@
 package com.company;
 import java.util.Scanner;
-
 import static com.company.Order.*;
 
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Pizza Project! You will be making you own Pizza!");
         orderCrust(sc);
